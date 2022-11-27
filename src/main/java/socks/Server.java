8baf174d78e9c71347200d1745f7d896da7e227c
raf <rraf@tuta.io>
2022-11-27 07:29:20 +0000

    update: initialised java project

---
+++ src/main/java/socks/Server.java
@@ -0,0 +1,9 @@
+package socks;
+
+public class Server
+{
+  public static void main(String[] args)
+  {
+    System.out.println("i will be a multithreaded client server socket chat app which uses json");
+  }
+}
