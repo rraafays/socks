e90ed90cfe0d373228fe5e1ff4b87c9f18d3f46e
raf <rraf@tuta.io>
2022-11-27 11:06:07 +0000

    chore: test to see if sh script can load classpath correctly

--- src/main/java/socks/Server.java
+++ src/main/java/socks/Server.java
@@ -1,9 +1,12 @@
 package socks;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class Server
 {
   public static void main(String[] args)
   {
     System.out.println("i will be a multithreaded client server socket chat app which uses json");
+    ObjectMapper mapper = new ObjectMapper();
   }
 }
