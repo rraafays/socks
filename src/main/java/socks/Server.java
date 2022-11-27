e7bccc74b3b0999af5b0da260124c54a9c893a4d
raf <rraf@tuta.io>
2022-11-27 13:02:21 +0000

    chore: test using json to create a car object

--- src/main/java/socks/Server.java
+++ src/main/java/socks/Server.java
@@ -1,12 +1,22 @@
 package socks;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Server
 {
+  static ObjectMapper mapper = new ObjectMapper();
+
   public static void main(String[] args)
   {
-    System.out.println("i will be a multithreaded client server socket chat app which uses json");
-    ObjectMapper mapper = new ObjectMapper();
+    String json = "{\"brand\":\"Porsche\", \"model\":\"Macan\"}";
+    try
+    { 
+      Car car = mapper.readValue(json, Car.class); 
+      System.out.println(car.model);
+    }
+    catch (JsonProcessingException e) { e.printStackTrace(); }
   }
 }
+
+class Car { public String brand; public String model; }
