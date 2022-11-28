c03f907b7543ba3631054b08c850c97fef679e36
raf <rraf@tuta.io>
2022-11-28 15:57:50 +0000

    chore: created json object for the client to receive from server

--- src/main/java/socks/Client.java
+++ src/main/java/socks/Client.java
@@ -1,9 +1,14 @@
 package socks;
 
+// objects to map json sent by the server into
+class SuccessResponse { public String _class; }
+class ErrorResponse { public String _class; public String error; }
+class MessageListResponse { public String _class; public Message[] messages; }
+
 public class Client
 {
   public static void main(String[] args)
   {
-    System.out.println("client");
+
   }
 }
