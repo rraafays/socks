039fc8c02d53f96240d38b4c138aae9b2870b713
raf <rraf@tuta.io>
2022-11-29 14:23:59 +0000

    update: finished wrote sender thread

--- src/main/java/socks/Client.java
+++ src/main/java/socks/Client.java
@@ -9,6 +9,6 @@ public class Client
 {
   public static void main(String[] args)
   {
-
+    System.out.println("bruh");
   }
 }
--- src/main/java/socks/Server.java
+++ src/main/java/socks/Server.java
@@ -35,29 +35,51 @@ public class Server
     // 4 - in case of get, server responds with messagelist otherwise server responds with success or error
     // 5 - loop 3
 
-
-    final int PORT = 12345;
-    ServerSocket ss;
-    Socket s;
-    BufferedReader i;
-    PrintWriter o;
+    final int PORT = 12345; // constant port number
+    ServerSocket ss; // server socket object
+    Socket s; // socket object
+    BufferedReader i; // buffered reader for socket
+    PrintWriter o; // print writer for writing data into the socket
     Scanner sc = new Scanner(System.in);
 
-    try 
+    try // try to instantiate our objects
     { 
-      ss = new ServerSocket(PORT); 
-      s = ss.accept();
-      o = new PrintWriter(s.getOutputStream());
-      i = new BufferedReader(new InputStreamReader(s.getInputStream()));
-    }
-    catch (IOException e) { e.printStackTrace(); }
+      ss = new ServerSocket(PORT); // server socket constructor requires port number
+      s = ss.accept(); // server socket accept method to wait for and accept connections
+      o = new PrintWriter(s.getOutputStream()); // create print writer from client output stream
+      i = new BufferedReader(new InputStreamReader(s.getInputStream())); // create buffered reader from client input stream
 
-    Thread sender = new Thread(new Runnable()
-    {
-      while(true)
+      Thread sender = new Thread(new Runnable() 
       {
-        
-      }
-    });
+        String json; // variable that will contain data written by client
+        @Override // override the run method
+        public void run()
+        {
+          while(true)
+          {
+            json = sc.nextLine(); // reads data from client terminal
+            o.println(json); // write data stored in msg
+            o.flush(); // flushes the printwriter
+          }
+        }
+      });
+
+      Thread listener = new Thread(new Runnable() 
+      {
+        String json;
+        @Override
+        public void run()
+        {
+          try
+          {
+            
+          }
+          catch
+        }
+      });
+      sender.start(); // starts the sender
+    }
+    catch (IOException e) { e.printStackTrace(); } // if any input output errors occour, print them
+
   }
 }
