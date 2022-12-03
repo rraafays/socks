ff661f84c9518861676a8db41059bf8ffdfefd18
raf <rraf@tuta.io>
2022-12-03 08:57:55 +0000

    update: working open request example

--- src/main/java/socks/Client.java
+++ src/main/java/socks/Client.java
@@ -15,6 +15,7 @@ import java.util.Scanner;
 
 // jackson json library
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 // objects to map json sent by the server into
 class SuccessResponse { public String _class; }
@@ -23,8 +24,21 @@ class MessageListResponse { public String _class; public Message[] messages; }
 
 public class Client
 {
+  static Scanner scanner = new Scanner(System.in);
+  static ObjectMapper mapper = new ObjectMapper();
+
   public static void main(String[] args)
   {
-    System.out.println("i will be the client for a client server socket chat app which uses json");
+    System.out.println("Enter identity: ");
+    String identity = scanner.nextLine();
+    String open_request_json = "{\"_class\":\"OpenRequest\", \"identity\":\"" + identity + "\"}";
+
+    System.out.println(open_request_json);
+    try 
+    {
+      Open_Request open_request =  mapper.readValue(open_request_json, Open_Request.class);
+      System.out.println(open_request.identity);
+    }
+    catch (JsonProcessingException e) { e.printStackTrace(); }
   }
 }
