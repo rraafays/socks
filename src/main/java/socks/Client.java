c70021e8b7e82b46b2ee87599cc51cd96e14462e
raf <rraf@tuta.io>
2022-12-27 21:02:02 +0000

    update: working subscribe request

--- src/main/java/socks/Client.java
+++ src/main/java/socks/Client.java
@@ -9,6 +9,7 @@ import java.io.BufferedReader;
 // network library
 import java.net.Socket;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 // jackson library
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -22,6 +23,7 @@ public class Client
   static ObjectMapper mapper = new ObjectMapper(); // json object mapper
   final static String ADDR = "localhost"; // constant address string
   final static int PORT = 12345; // constant port number
+  static String identity; // identity string
 
   public static void main(String[] args) throws IOException
   {
@@ -31,7 +33,7 @@ public class Client
     PrintWriter server = new PrintWriter(socket.getOutputStream(), true); // create printwriter from the socket's output stream and set it to autoflush
 
     System.out.println("Enter identity: "); // prompt client for an identity
-    String identity = client.readLine(); // read the client's input
+    identity = client.readLine(); // read the client's input
 
     Open_Request open_request = new Open_Request(); // create new open request
     open_request._class = "OpenRequest"; open_request.identity = identity; // initialise using client identity
@@ -43,7 +45,11 @@ public class Client
       
       String option = client.readLine(); // grab an argument number from the client's input
       if (option.equals("1")) { server.println(Publish()); } // if option is 1 then publish
-      if (option.equals("2")) { server.println(Subscribe()); } // if option is 2 then subscribe
+      if (option.equals("2")) // if option is 2 then subscribe
+      { 
+        System.out.println("Who would you like to subscribe to? "); // ask user who they would like to subscribe to
+        server.println(Subscribe(client.readLine())); // print subscribe request to the server where whatever they type is the channel they wish to subscribe to
+      } 
       if (option.equals("3")) { server.println(Get()); } // if option is 3 then get messages
     }
   }
@@ -60,9 +66,15 @@ public class Client
     return("1 is not implemented!");
   }
 
-  static String Subscribe() // build subscribe request
+  static String Subscribe(String channel) // build subscribe request
   {
-    return("{\"_class\":\"SubscribeRequest\", \"identity\":\"Alice\", \"channel\":\"Bob\"}");
+    Subscribe_Request subscribe_request = new Subscribe_Request(); // create subscribe request object
+    subscribe_request._class = "SubscribeRequest"; // set the _class
+    subscribe_request.identity = identity; // set identity
+    subscribe_request.channel = channel; // set channel
+    
+    try { return(mapper.writeValueAsString(subscribe_request)); } // try to return the subscribe request as json string
+    catch (JsonProcessingException error) { return(""); } // if any errors occour, return an empty string
   }
 
   static String Get() // build get request
